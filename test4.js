79859d7d397938ea9751cfd01f7c267f152cb0f2
dilsaster <fdlhq@dilsasters-MacBook-Air.local>
2023-08-02 15:58:59 +0700

    Fix conflicts in test4.js

--- test4.js
+++ test4.js
@@ -1,19 +1,18 @@
 function indexPrime (param1) {
-    let primaNum = []
-    for(let i = 2; ; i++) {
-        let bilPrim = false;
-        for(let j = 2; j < i ; j++) {
-        if(i % j == 0) {
-        bilPrim = true; 
-        } 
-    }
-    if(bilPrim == false) {
-            primaNum.push(i)
-        } if(param1 == primaNum.length){
+    let hasil = []
+    for (i = 2; ; i++) {
+        let biPrim = true
+        for (j = 2; j < i; j++) {
+        if (i % j == 0) {
+        biPrim = false;
+        }} if (biPrim == true) {
+        hasil.push(i);
+    } if (param1 == hasil.length) {
         break;
-        }  
-}
-    return primaNum[param1 -1] 
+    }
+    }
+
+    return hasil[param1 -1];
 }
 
 console.log(indexPrime(4));
