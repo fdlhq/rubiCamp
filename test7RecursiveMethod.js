eead234ba09dc4f2fa9af879177fb8230aefc16e
dilsaster <fdlhq@dilsasters-MacBook-Air.local>
2023-08-04 09:55:13 +0700

    tugas 7 dengan recursive method

---
+++ test7RecursiveMethod.js
@@ -0,0 +1,18 @@
+function weirdMultiple(num) {
+  if (num < 10) {
+    return num;
+  }
+
+  const A = num.toString().split("");
+  let hasil = 1;
+
+  for (let i = 0; i < A.length; i++) {
+    hasil *= A[i];
+    console.log(hasil);
+  }
+  return weirdMultiple(hasil);
+}
+
+console.log(weirdMultiple(39));
+// console.log(weirdMultiple(999));
+// console.log(weirdMultiple(3));
